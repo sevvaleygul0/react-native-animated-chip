5f8de78a92af01010daf8ad96164ff7fafe2fa72
sevvaleygul0 <sevvalleygull@gmail.com>
2024-01-21 20:38:18 +0300

    feat: reanimated 3 library integration

---
+++ example/lib/AnimatedChip/AnimatedChip.tsx
@@ -0,0 +1,14 @@
+import React, {useState} from 'react';
+import {View} from 'react-native';
+/**
+ * ? Local Imports
+ */
+import styles from './AnimatedChip.style';
+
+interface IAnimatedChip {}
+
+const AnimatedChip: React.FC<IAnimatedChip> = ({}) => {
+  return <View style={styles.container}></View>;
+};
+
+export default AnimatedChip;
---
+++ example/lib/index.ts
@@ -0,0 +1,4 @@
+import Text from "./AnimatedChip/AnimatedChip";
+import { Hello } from "./StateFulComponent/Hello";
+
+export { Text, Hello };
